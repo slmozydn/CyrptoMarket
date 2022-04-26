2ff5883e5d44388ec8f41ad9637c46c8b02f1322
selim <35482253+slmozydn@users.noreply.github.com>
2022-04-26 03:12:17 +0300

    Implement view bindings

--- app/src/main/java/com/selim/cyrptomarket/ui/HomeFragment.kt
+++ app/src/main/java/com/selim/cyrptomarket/ui/HomeFragment.kt
@@ -1,7 +1,24 @@
 package com.selim.cyrptomarket.ui
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.selim.cyrptomarket.R
+import com.selim.cyrptomarket.databinding.FragmentHomeBinding
+import com.selim.cyrptomarket.util.viewBinding
 
-class HomeFragment: Fragment(R.layout.fragment_home) {
+class HomeFragment : Fragment(R.layout.fragment_home) {
+
+    private val binding: FragmentHomeBinding by viewBinding(FragmentHomeBinding::bind)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        with(binding) {
+            button.setOnClickListener {
+                findNavController().navigate(HomeFragmentDirections.actionHomeFragmentToSearchFragment())
+            }
+        }
+    }
+
 }
--- app/src/main/java/com/selim/cyrptomarket/ui/MainActivity.kt
+++ app/src/main/java/com/selim/cyrptomarket/ui/MainActivity.kt
@@ -2,13 +2,27 @@ package com.selim.cyrptomarket.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.selim.cyrptomarket.R
 import com.selim.cyrptomarket.R.layout
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(layout.activity_main) {
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(layout.activity_main)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHostFragment.navController
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
     }
+
+    override fun onSupportNavigateUp() = navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
 }
