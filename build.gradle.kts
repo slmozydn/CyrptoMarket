27a0421afb5981c7455a4f65b51d04ee2f688e90
selim <35482253+slmozydn@users.noreply.github.com>
2022-04-27 05:49:36 +0300

    Use kotlin dsl

---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+buildscript {
+  repositories {
+    google()
+    mavenCentral()
+  }
+  dependencies {
+    classpath("com.android.tools.build:gradle:7.1.3")
+    classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
+    classpath("com.google.dagger:hilt-android-gradle-plugin:2.41")
+    classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.4.2")
+  }
+}
+
+allprojects {
+  repositories {
+    google()
+    mavenCentral()
+  }
+}
+
+task("clean", Delete::class) {
+  delete(rootProject.buildDir)
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,2 @@
 rootProject.name = "CyrptoMarket"
-include ':app'
+include(":app")
